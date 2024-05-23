c04cad88db86ed38e9a473a0d80e817cee5c52bc
Gustavo Resende <gustavo.reseende@gmail.com>
2024-05-22 23:08:04 -0300

    ToDoList com espaço para novas implementacoes!

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Program
+{
+    public class Program()
+    {
+        public static void Main()
+        {
+            List<string> lista = new List<string>();
+            while (true)
+            {
+                Console.Write("Adicionar, remover ou tarefas: ");
+                string acao = Console.ReadLine();
+
+                Console.Clear();
+                
+                if (acao == "Adicionar"|| acao == "adicionar")
+                {                   
+                    Console.Write("Nome da tarefa: ");
+                    string tarefa = Console.ReadLine();
+
+                    lista.Add(tarefa);
+
+                    foreach (var task in lista)
+                    {
+                        Console.WriteLine($"- {task}");
+                    }
+                }
+
+                if (acao == "Remover" || acao == "remover")
+                {
+                    Console.Write("Remover tarefa: ");
+                    string tarefa = Console.ReadLine();
+
+                    lista.Remove(tarefa);
+                    foreach (var task in lista)
+                    {
+                        Console.WriteLine($"- {task}");
+                    }
+                }
+
+                if (acao == "Tarefas" || acao == "tarefas")
+                {
+                    foreach (var task in lista)
+                    {
+                        Console.WriteLine($"- {task}");
+                    }
+                }      
+            }
+        }
+    }
+} 
